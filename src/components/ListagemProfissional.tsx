6ed809ae38e8f663ae9c5088f4fa7a1194b435eb
jaozinhodagarapa <joaovictorribeirooliveira49@gmail.com>
2023-10-31 15:24:23 -0300

    Atualizado

--- src/components/ListagemProfissional.tsx
+++ src/components/ListagemProfissional.tsx
@@ -50,7 +50,7 @@ const ListagemProfissionais = () => {
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await axios.get('http://127.0.0.1:8000/api/retornartodosProfissionais');
+                const response = await axios.get('http://127.0.0.1:8000/api/retornarTodos');
                 console.log(response.data.data);
                 setProfissionais(response.data.data);
             } catch (error) {
